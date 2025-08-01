54946f507a6309e0a29db2eaf2ded90562404cef
shihx2003 <shihx2003@outlook.com>
2025-08-01 22:25:20 +0800

    load date

---
+++ util/read_obs.py
@@ -0,0 +1,42 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   read_obs.py
+@Create  :   2025-08-01 21:55:41
+@Author  :   shihx2003
+@Version :   1.0
+@Contact :   shihx2003@outlook.com
+'''
+
+# here put the import lib
+import pandas as pd
+
+def load_qobs(basin, **kwargs) -> pd.DataFrame:
+    """
+    """
+    obs_dir = kwargs.get('obs_dir', 'E:/Working/QingHaiSnow/CalRsim/Source/Qobs/')
+    df = pd.read_csv(obs_dir + f'{basin}_qobs.csv')
+    df.rename(columns={'Qobs': 'qobs'}, inplace=True)
+    df['Date'] = pd.to_datetime(df['Date'])
+
+    return df
+
+def div_q(df, start, end) -> pd.DataFrame:
+
+    df = df.copy()
+    
+    start = pd.to_datetime(start)
+    end = pd.to_datetime(end)
+
+    if not pd.api.types.is_datetime64_any_dtype(df['Date']):
+        df['Date'] = pd.to_datetime(df['Date'])
+    
+    mask = (df['Date'] >= start) & (df['Date'] <= end)
+    mask_df = df[mask].reset_index(drop=True)
+
+    return mask_df
+
+if __name__ == "__main__":
+    df = load_qobs('buhahk')
+    print(df)
+    dfk = div_q(df, '2015-01-01', '2022-12-31')
+    print(dfk)
---
+++ util/read_sim.py
@@ -0,0 +1,57 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   read_StaQSim.py
+@Create  :   2025-08-01 20:29:55
+@Author  :   shihx2003
+@Version :   1.0
+@Contact :   shihx2003@outlook.com
+'''
+
+# here put the import lib
+import os
+import yaml
+import pandas as pd
+import numpy as np
+
+def read_sta_qsim(StaQSim_path: str) -> pd.DataFrame:
+
+    colspecs = [(0, 11), (11, 21), (21, 31), (31, 46), (46, 61), (61, 76), (76, 91)]
+    df = pd.read_fwf(StaQSim_path, colspecs=colspecs, header=None)
+    df.columns = ['Year', 'Month', 'Day', 'Precipitation', 'Snowmelt_Liquid', 'Historical_Runoff', 'Sim_Q']
+    df['Date'] = pd.to_datetime(df[['Year', 'Month', 'Day']])
+    df = df[['Date', 'Precipitation', 'Snowmelt_Liquid', 'Historical_Runoff', 'Sim_Q']]
+    df.replace('***************', 9999999999.0000, inplace=True)
+    
+    df['Date'] = pd.to_datetime(df['Date'])
+    return df
+
+def load_qsim(job_yaml, basin, **kwargs) -> pd.DataFrame:
+
+    result_dir = kwargs.get('result_dir', 'E:/Working/QingHaiSnow/CalRsim/Results/')
+
+    with open(job_yaml, 'r', encoding='utf-8') as f:
+        job_config = yaml.safe_load(f)
+    job_ids = list(job_config.keys())
+
+    qsim_dfs = []
+    for job_id in job_ids:
+        file_path = os.path.join(result_dir, basin, f"StaQSim_{job_id}.txt")
+        df = read_sta_qsim(file_path)
+        df = df[['Date', 'Sim_Q']]
+        df = df.rename(columns={'Sim_Q': job_id})
+
+        if not qsim_dfs:
+            qsim_dfs.append(df)
+        else:
+            qsim_dfs.append(df[[job_id]])
+            
+    qsim_df = pd.concat(qsim_dfs, axis=1)
+
+    df['Date'] = pd.to_datetime(df['Date'])
+    return qsim_df
+
+if __name__ == "__main__":
+    job_yaml = '.\\jobs\\iRainSnowJob_buhahk_test_1.yaml'
+    df = load_qsim(job_yaml, 'buhahk2')
+    print(df)
+    df.to_csv('E:/Working/QingHaiSnow/CalRsim/test/buhahk_qsim.csv', index=False)
